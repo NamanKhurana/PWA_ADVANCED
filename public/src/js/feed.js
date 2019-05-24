02b01395a070e1143749222193adfe2ce0ec6b12
NamanKhurana <khurananaman06@gmail.com>
2019-05-24 23:49:16 +0530

    updated with caching
    Signed-off-by: NamanKhurana <khurananaman06@gmail.com>

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -6,26 +6,33 @@ var sharedMomentsArea = document.getElementById('shared-moments')
 function openCreatePostModal() {
   createPostArea.style.display = 'block';
 
-  if(deferredPrompt)
-  {
+  if (deferredPrompt) {
     deferredPrompt.prompt()
 
-    deferredPrompt.userChoice.then(function(choiceResult){
+    deferredPrompt.userChoice.then(function (choiceResult) {
       console.log(choiceResult.outcome)
 
-      if(choiceResult.outcome == 'dismissed')
-      {
+      if (choiceResult.outcome == 'dismissed') {
         console.log('User cancelled installation')
       }
-      else
-      {
+      else {
         console.log('User added to home screen')
       }
-   
+
     })
 
     deferredPrompt = null
   }
+
+  // if('serviceWorker' in navigator){
+  //   navigator.serviceWorker.getRegistrations()
+  //   .then(function(registrations){
+  //     for(var i = 0; i < registrations.length ; i++)
+  //     {
+  //       registrations[i].unregister()
+  //     }
+  //   })
+  // }
 
 
 }
@@ -38,41 +45,112 @@ shareImageButton.addEventListener('click', openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
 
-function onSaveButtonClicked(event)
-{
+//Function used to save assets in cache on clicking a button
+function onSaveButtonClicked(event) {
   console.log("CLICKED")
+
+  if ('caches' in window) {
+    caches.open('user-requested')
+      .then(function (cache) {
+        cache.add('https://httpbin.org/get')
+        cache.add('/public/src/images/sf-boat.jpg')
+      })
+  }
 }
 
-function createCard() {
+function clearCards() {
+  while (sharedMomentsArea.hasChildNodes()) {
+    sharedMomentsArea.removeChild(sharedMomentsArea.lastChild)
+  }
+}
+
+function createCard(data) {
   var cardWrapper = document.createElement('div');
   cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
   var cardTitle = document.createElement('div');
   cardTitle.className = 'mdl-card__title';
-  cardTitle.style.backgroundImage = 'url("/public/src/images/sf-boat.jpg")';
+  cardTitle.style.backgroundImage = "url(" + data.image + ")";
   cardTitle.style.backgroundSize = 'cover';
   cardTitle.style.height = '180px';
   cardWrapper.appendChild(cardTitle);
   var cardTitleTextElement = document.createElement('h2');
   cardTitleTextElement.className = 'mdl-card__title-text';
-  cardTitleTextElement.textContent = 'San Francisco Trip';
+  cardTitleTextElement.textContent = data.title;
   cardTitle.appendChild(cardTitleTextElement);
   var cardSupportingText = document.createElement('div');
   cardSupportingText.className = 'mdl-card__supporting-text';
-  cardSupportingText.textContent = 'In San Francisco';
+  cardSupportingText.textContent = data.location;
   cardSupportingText.style.textAlign = 'center';
-  var cardSaveButton = document.createElement('button')
-  cardSaveButton.textContent = 'Save'
-  cardSaveButton.addEventListener('click',onSaveButtonClicked)
-  cardSupportingText.appendChild(cardSaveButton)
+  // var cardSaveButton = document.createElement('button')
+  // cardSaveButton.textContent = 'Save'
+  // cardSaveButton.addEventListener('click',onSaveButtonClicked)
+  // cardSupportingText.appendChild(cardSaveButton)
   cardWrapper.appendChild(cardSupportingText);
   componentHandler.upgradeElement(cardWrapper);
   sharedMomentsArea.appendChild(cardWrapper);
 }
 
-fetch('https://httpbin.org/get')
-  .then(function(res) {
+function updateUI(data) {
+  clearCards()
+  for (var i = 0; i < data.length; i++) {
+    createCard(data[i])
+  }
+}
+
+var url = "https://pwa-advanced.firebaseio.com/posts.json"
+var networkDataRecieved = false
+
+fetch(url)
+  .then(function (res) {
     return res.json();
   })
-  .then(function(data) {
-    createCard();
+  .then(function (data) {
+    networkDataRecieved = true
+    console.log("FROM WEB", data)
+    var dataArray = []
+    for (var key in data) {
+      dataArray.push(data[key])
+    }
+    updateUI(dataArray)
   });
+
+// fetch(url,{
+//   method:'POST',
+//   headers:{
+//     'Content-Type':'application/json',
+//     'Accept':'application/json' 
+//   },
+//   body:JSON.stringify({
+//     message:'Some message'
+//   })
+// })
+//   .then(function (res) {
+//     return res.json();
+//   })
+//   .then(function (data) {
+//     networkDataRecieved = true
+//     console.log("FROM WEB", data)
+//     clearCards()
+//     createCard();
+//   });
+
+
+if ('caches' in window) {
+  caches.match(url)
+    .then(function (response) {
+      if (response) {
+        return response.json()
+      }
+    })
+    .then(function (data) {
+      console.log("FROM CACHE", data)
+      if (!networkDataRecieved) {
+        var dataArray = []
+        for (var key in data) {
+          dataArray.push(data[key])
+        }
+        updateUI(dataArray)
+      }
+    })
+}
+
--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,34 @@
-var CACHE_STATIC_NAME = 'static-v5'
+var CACHE_STATIC_NAME = 'static-v15'
 var CACHE_DYNAMIC_NAME = 'dynamic-v2'
+var STATIC_FILES = [
+    '/public/index.html',
+    '/public/offline.html',
+    '/public/src/app.js',
+    '/public/src/js/feed.js',
+    '/public/src/js/promise.js',
+    '/public/src/js/fetch.js',
+    '/public/src/js/material.min.js',
+    '/public/src/css/app.css',
+    '/public/src/css/feed.css',
+    '/public/src/images/main-image.jpg',
+    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+]
+
+//CLEAN DYNAMIC CACHE
+function trimCache(cacheName, maxItems) {
+    caches.open(cacheName)
+        .then(function (cache) {
+            return cache.keys()
+                .then(function (keys) {
+                    if (keys.length > maxItems) {
+                        cache.delete(keys[0])
+                            .then(trimCache(cacheName, maxItems))
+                    }
+                })
+        })
+}
 
 self.addEventListener("install", function (event) {
     console.log("Installing service worker.. (Triggered by browser)", event)
@@ -26,6 +55,7 @@ self.addEventListener("install", function (event) {
 
                 //cache.add('/')
                 cache.add('/public/index.html')
+                cache.add('/public/offline.html')
                 cache.add('/public/src/js/app.js')
                 cache.add('/public/src/js/feed.js')
                 cache.add('/public/src/js/promise.js')
@@ -46,40 +76,162 @@ self.addEventListener("activate", function (event) {
 
     event.waitUntil(
         caches.keys()
-        .then(function(keylist){
-            return Promise.all(keylist.map(function(key){
-                if(key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME){
-                    console.log('Removing Old Cache' , key)
-                    return caches.delete(key)
-                }
-            }))
-        })
+            .then(function (keylist) {
+                return Promise.all(keylist.map(function (key) {
+                    if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+                        console.log('Removing Old Cache', key)
+                        return caches.delete(key)
+                    }
+                }))
+            })
     )
 
     return self.clients.claim()
 })
 
+// ---> CACHE WITH NETWORK FALLBACK <---
+// self.addEventListener("fetch", function (event) {
+//     event.respondWith(
+//         caches.match(event.request)
+//             .then(function (response) {
+//                 if (response) {
+//                     return response
+//                 }
+//                 else {
+//                     return fetch(event.request)
+//                         .then(function (res) {
+//                             caches.open(CACHE_DYNAMIC_NAME)
+//                                 .then(function (cache) {
+//                                   cache.put(event.request.url, res.clone())
+//                                     return res
+//                                 })
+//                         })
+//                         .catch(function(err){ 
+//                          return caches.open(CACHE_STATIC_NAME)
+//                             .then(function(cache){
+//                                 return cache.match('/public/offline.html')
+//                             })
+//                         })
+//                 }
+//             })
+//     )
+
+// })
+
+// // ---> CACHE THEN NETWORK  (IMPROVED VERSION OF NETWORK WITH CACHE FALLBACK) <---
+// self.addEventListener("fetch", function (event) {
+//     event.respondWith(
+//       caches.open(CACHE_DYNAMIC_NAME)
+//       .then(function(cache){
+//           return fetch(event.request)
+//           .then(function(res){
+//               cache.put(event.request,res.clone())
+//               return res
+//           })
+//       })
+//     )
+
+// })
+
+
+function isInArray(string, array) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] === string) {
+            return true
+        }
+    }
+    return false
+}
+
+// ---> IMPORVED VERSION OF CACHE THEN NETWORK <---
 self.addEventListener("fetch", function (event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function (response) {
-                if (response) {
-                    return response
-                }
-                else {
+    var url = "https://pwa-advanced.firebaseio.com/posts"
+
+    if (event.request.url.indexOf(url) > -1) {
+        event.respondWith(
+            caches.open(CACHE_DYNAMIC_NAME)
+                .then(function (cache) {
                     return fetch(event.request)
                         .then(function (res) {
-                            caches.open(CACHE_DYNAMIC_NAME)
-                                .then(function (cache) {
-                                 // cache.put(event.request.url, res.clone())
-                                    return res
-                                })
+                            // trimCache(CACHE_DYNAMIC_NAME,5)
+                            cache.put(event.request, res.clone())
+                            return res
                         })
-                        .catch(function(err){
+                })
+        )
 
-                        })
-                }
-            })
-    )
+    }
+    // } else if (new RegExp('\\b' + STATIC_FILES.join('\\b|\\b') + '\\b').test(event.request.url)) {
+
+    else if (isInArray(event.request.url, STATIC_FILES)) {
+        event.respondWith(
+            caches.match(event.request)
+        )
+    }
+
+    else {
+        event.respondWith(
+
+            caches.match(event.request)
+                .then(function (response) {
+                    if (response) {
+                        return response
+                    }
+                    else {
+                        return fetch(event.request)
+                            .then(function (res) {
+                                caches.open(CACHE_DYNAMIC_NAME)
+                                    .then(function (cache) {
+                                        cache.put(event.request.url, res.clone())
+                                        return res
+                                    })
+                            })
+                            .catch(function (err) {
+                                return caches.open(CACHE_STATIC_NAME)
+                                    .then(function (cache) {
+                                        // if (event.request.url.indexOf('/help'))
+                                        if (event.request.headers.get('accept').includes('text/html'))
+                                            return cache.match('/public/offline.html')
+                                    })
+                            })
+                    }
+                })
+
+        )
+    }
 
 })
+
+
+// ---> NETWORK WITH CACHE FALLBACK <---
+// self.addEventListener("fetch",function(event){
+//     event.respondWith(
+//         fetch(event.request)
+//         .then(function(res){
+//            return caches.open(CACHE_DYNAMIC_NAME)
+//            .then(function(cache){
+//                cache.put(event.request,res.clone())
+//                return res
+//            })
+//         })
+//         .catch(function(err){
+//             return caches.match(event.request)
+//         })
+//         )
+// })
+
+// ---> CACHE ONLY <---
+// self.addEventListener("fetch", function (event) {
+//     event.respondWith(
+//         caches.match(event.request)
+//     )
+
+// })
+
+// ---> NETWORK ONLY <---
+// self.addEventListener("fetch", function (event) {
+//     event.respondWith(
+//         fetch(event.request)
+//     )
+
+// })
